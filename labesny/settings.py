d036b90eff08b76f2ede7c550741fbfccd399b4a
42018067 <42018067@hti.edu.eg>
2022-01-22 22:01:08 +0200

    Added all files

--- labesny/settings.py
+++ labesny/settings.py
@@ -20,12 +20,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%)o3@wr4&)^^%r-&u!y&=@*z1-hagb%+06^!x1!sao^g^v8g$!'
-
+SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -126,8 +125,8 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-REST_FRAMEWORK={
-    "DEFAULT_AUTHENTICATION_CLASSES":('knox.auth.TokenAuthentication',)
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': ('knox.auth.TokenAuthentication',)
 }
 
 django_heroku.settings(locals())
